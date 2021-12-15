79bf9c25e4e265de75d67b22c77cc350806cd47e
zhaojie <1979170837@qq,com>
2021-12-15 20:01:02 +0800

    initialize.

---
+++ nacos-demo/src/main/java/nacos/demo/NacosDemo.java
@@ -0,0 +1,15 @@
+package nacos.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Jie Zhao
+ * @date 2021/12/15 19:59
+ */
+@Slf4j
+public class NacosDemo {
+
+    public static void main(String[] args) {
+        log.info("Hello Nacos!!!");
+    }
+}
